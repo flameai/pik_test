9e34069b1d5dd9fb467d4c9c11df7729624a58ef
Alexander Andryukov <andryukov@gmail.com>
2021-08-09 12:01:44 +0500

    serializer validation

--- main/serializers.py
+++ main/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from main.models import *
+from django.core.exceptions import ValidationError
+
 
 class ZoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Zone
         fields = '__all__'
 
+    def validate(self, data):
+        tmp_obj = Zone(**data)
+        try:
+            tmp_obj.clean()
+        except ValidationError as e:
+            raise serializers.ValidationError(e)
