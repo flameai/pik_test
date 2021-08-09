e0a04df25d3ad24629ae39c3f5a71189e36c5dae
Alexander Andryukov <andryukov@gmail.com>
2021-08-09 14:47:19 +0500

    point action

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,28 @@
 from rest_framework import serializers
-from main.models import *
+from main.models import Partner, Zone, Service, ServiceType
 from django.core.exceptions import ValidationError
 
 
+class ServiceTypeSerializer(serializer.ModelSerializer):
+    class Meta:
+        models = ServiceType
+        fields = '__all__'
+
+class ServiceSerializer(serializers.ModelSerializer):
+    service_type = ServiceTypeSerializer(many=True)
+    class Meta:
+        model = Service
+        fields = '__all__'
+
+class PartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Partner
+        fields = '__all__'
+
 class ZoneSerializer(serializers.ModelSerializer):
+    services = ServiceSerializer(many=True)
+    partner = PartnerSerializer()
+
     class Meta:
         model = Zone
         fields = '__all__'
--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from main.models import Zone
 from main.serializers import ZoneSerializer
 from django_filters import rest_framework as filters
+from django.contrib.gis.geos import Point
 
 # Create your views here.
 
@@ -9,6 +11,15 @@ class ZoneViewSet(ModelViewSet):
     serializer_class = ZoneSerializer
     queryset = Zone.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
-    # filterset_fields = ('category', 'in_stock')
-    def get_queryset(self):
-        return super().get_queryset().filter(provider__manager=self.request.user)
+    filterset_fields = ('services__service_type',)
+
+    # def get_queryset(self):
+    #     return super().get_queryset().filter(provider__manager=self.request.user)
+
+    @action(methods=['get',], detail=False)
+    def point(self, request, *args, **kwargs):
+        longitude = float(request.GET.get('long', '0'))
+        latitude = float(request.GET.get('lat', '0'))
+        point = Point(longitude, latitude)
+        self.queryset = self.queryset.filter(mpoly__contains=point)
+        return super().list(request, *args, **kwargs)
