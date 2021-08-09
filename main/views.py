cff5e6568c72754d8f2293bc68fac4f30551ad8b
Alexander Andryukov <andryukov@gmail.com>
2021-08-09 12:46:40 +0500

    qs + df

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from main.models import Zone
 from main.serializers import ZoneSerializer
+from django_filters import rest_framework as filters
 
 # Create your views here.
 
 class ZoneViewSet(ModelViewSet):
     serializer_class = ZoneSerializer
     queryset = Zone.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    # filterset_fields = ('category', 'in_stock')
     def get_queryset(self):
-        return super().get_queryset().filter(user=self.request.user)
+        return super().get_queryset().filter(provider__manager=self.request.user)
