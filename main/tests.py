fb830ef4f2b4ca8ea2dcdfd7c5f83dc5de5239eb
Alexander Andryukov <andryukov@gmail.com>
2021-08-11 21:11:32 +0500

    test2

--- main/tests.py
+++ main/tests.py
@@ -5,6 +5,7 @@ from main.views import ProviderViewSet
 
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import force_authenticate
+from main import serializers
 
 
 # Create your tests here.
@@ -26,14 +27,21 @@ class CreateProvider(TestCase):
         vasya_provider = Provider.objects.get(pk=1)
         self.assertEqual(vasya_provider.manager, self.vasya)        
     
-    def test_users_can_update_provider(self):
-        """Пользователь может изменить поля поставщика """        
-        factory = APIRequestFactory()        
+    def test_users_can_update_provider_through_serializer(self):
+        """Пользователь может изменить поля поставщика через сериалайзер"""        
+        factory = APIRequestFactory()
+        # Создадим поставщика
+        view = ProviderViewSet.as_view({'post':'create'})
+        data = {'name': 'Васин новый поставщик', 'email': self.vasya.email, 'address':'Ленина 129', 'phone': '323233422'}
+        request = factory.post('/providers/', data, format='json')
+        force_authenticate(request, user=self.vasya)
+        view(request)
         # Изменим поставщика
         view = ProviderViewSet.as_view({'patch':'partial_update'})
         new_data = {'name': '(Обновлено) Васин новый поставщик', 'email': 'new_valid@email.com', 'address':'(Обновлено)Ленина 129', 'phone': '123'}
         request = factory.patch('/providers/1/', new_data, format='json')
         force_authenticate(request, user=self.vasya)
-        
-        
-        self.assertEqual(response.status_code, 200)
+        provider = Provider.objects.get(pk=2)
+        serializer = serializers.ProviderSerializer(
+            provider, data=new_data)        
+        self.assertEqual(serializer.is_valid(), True)
